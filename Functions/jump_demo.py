d1e512f70cd1f0a3aa30be67bbc6a91a7571e853
Fabeius <fabio.buecheler@students.unibe.ch>
2023-10-03 11:06:39 +0200

    changed code in jump_demo

--- Functions/jump_demo.py
+++ Functions/jump_demo.py
@@ -1,15 +1,16 @@
 from turtle import forward, left, right, exitonclick
 
-def jump(size):
-    forward(size)
+def jump(hight=30):
     left(75)
-    forward(size)
+    forward(hight)
     right(150)
-    forward(size)
+    forward(hight)
     left(75)
 
-for size in range (10,70,5):    #die size variable wird von 10 bis 70 in 5er Schritten gezählt - demensprechend werden die forward grössen immer verändert/grösser
+for size in range (20,70,5):    #die size variable wird von 10 bis 70 in 5er Schritten gezählt - demensprechend werden die forward grössen immer verändert/grösser
+    forward(30)
     jump(size)
+
 
 exitonclick()
 
